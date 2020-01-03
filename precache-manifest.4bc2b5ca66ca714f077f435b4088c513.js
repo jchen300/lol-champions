b5f624d8a22dca934af99f0a256b6b0e95001a12
Jian Chen <jchen300@illinois.edu>
2020-01-03 16:16:39 -0600

    Updates

---
+++ precache-manifest.4bc2b5ca66ca714f077f435b4088c513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4402ceaf73296de671ec5e006a76e410",
+    "url": "/lol-champions/index.html"
+  },
+  {
+    "revision": "e076606464e2d3c00c9c",
+    "url": "/lol-champions/static/css/main.87faf9d2.chunk.css"
+  },
+  {
+    "revision": "6f2e0db982708f079b70",
+    "url": "/lol-champions/static/js/2.6709c717.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/lol-champions/static/js/2.6709c717.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e076606464e2d3c00c9c",
+    "url": "/lol-champions/static/js/main.09349056.chunk.js"
+  },
+  {
+    "revision": "d128d1abfc2f4ba3707e",
+    "url": "/lol-champions/static/js/runtime-main.fa8b44ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lol-champions/static/media/logo.5d5d9eef.svg"
+  }
+]);
